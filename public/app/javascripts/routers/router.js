c6866b064f66da97b52af59750d6b7acabe775e0
Anton Vasilenko <toxa07@gmail.com>
2014-09-11 00:48:11 +0300

    [task2]  - routing implemented

---
+++ public/app/javascripts/routers/router.js
@@ -0,0 +1,29 @@
+﻿/* global Backbone */
+var task2 = task2 || {};
+
+(function() {
+    'use strict';
+
+    // Film router
+    var FilmRouter = Backbone.Router.extend({
+        routes: {
+            '': "listFilms",
+            'film/:id': "filmDetails"
+        },
+
+        listFilms: function () {
+            $('#films-container').show();
+            $('#film-details-container').hide();
+            console.log('films list page called');
+        },
+
+        filmDetails: function (id) {
+            $('#films-container').hide();
+            $('#film-details-container').show();
+            console.log('details page called for film id '+ id );
+        }
+    });
+
+    task2.FilmRouter = new FilmRouter();
+    Backbone.history.start();
+})();
