fcfb07754142e7326e9017bd6941561aefca5b7d
Anton Vasilenko <toxa07@gmail.com>
2014-09-22 00:48:16 +0300

    [task4] - film collection reworked to use marionette

--- frontend/javascripts/film/FilmCollectionView.js
+++ frontend/javascripts/film/FilmCollectionView.js
@@ -1,42 +1,47 @@
-define(['backbone', 'marionette', './FilmView', './Film'],
+define(['backbone', 'marionette', 'film/FilmView', 'film/Film'],
 function (Backbone, Marionette, FilmView, Film) {
 
-    /*var FilmCollectionsView = Marionette.CompositeView.extend({
+    var FilmCollectionViewM = Marionette.CompositeView.extend({
         el: '#films-container',
         childViewContainer: "#films-list-container",
-        childView: FilmView
-    });*/
-
-    var FilmCollectionView = Backbone.View.extend({
-        el: '#films-container',
-        initialize: function () {
-            this.collection.on('add', this.renderNewFilm, this);
+        childView: FilmView,
+        ui: {
+            addName: "#film-add-name",
+            addYear: "#film-add-year",
+            list: "#films-list-container"
         },
+
         events: {
             'click button#add': 'addItem'
+        },
+
+        collectionEvents: {
+            'add': "renderNewFilm"
+        },
+
+        initialize: function() {
+            this.bindUIElements();
         },
 
         renderNewFilm: function (model) {
             var view = new FilmView({
                 model: model
             });
-            this.$el.find("#films-list-container").append(view.$el);
+            this.ui.list.append(view.$el);
         },
 
         addItem: function () {
-            var addName = this.$el.find("#film-add-name");
-            var addYear = this.$el.find("#film-add-year");
             var item = new Film({
-                name: addName.val(),
-                year: addYear.val()
+                name: this.ui.addName.val(),
+                year: this.ui.addYear.val()
             });
-            this.collection.create(item, {wait:true});
-            addName.val('');
-            addYear.val('');
+            this.collection.create(item, { wait: true });
+            this.ui.addName.val('');
+            this.ui.addYear.val('');
         },
 
     });
 
-    return FilmCollectionView;
+    return FilmCollectionViewM;
 });
 
